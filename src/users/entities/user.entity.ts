eb01af3eb6e761d0bc33504d9cbffe27cdd60b27
leekihun <hunit612@cntech.pro>
2023-08-23 17:29:21 +0900

    feat: connect mysql

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,25 @@
-export class User {}
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import * as argon2 from 'argon2';
+
+@Entity('user')
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 32 })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await argon2.hash(this.password);
+  }
+
+  @Column({ default: '' })
+  imageLink: string;
+}
